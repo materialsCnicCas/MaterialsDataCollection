5be9d7e15a33f148dff511c1456c1166e769513e
materialsCnicCas <145396189+materialsCnicCas@users.noreply.github.com>
2024-10-16 23:31:28 +0800

    Delete utils directory

--- utils/tools/calType.py
+++
@@ -1,8 +0,0 @@
-class CalType(enumerate):
-    BandStructure = 'BandStructure'
-    StaticCalculation = 'StaticCalculation'
-    GeometryOptimization = 'GeometryOptimization'
-    ElasticProperties = 'ElasticProperties'
-    MagneticProperties = 'MagneticProperties'
-    DensityOfStates = 'DensityOfStates'
-    DielectricProperties = 'DielectricProperties '
